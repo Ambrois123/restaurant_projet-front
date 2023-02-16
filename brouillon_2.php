e38071026eb252c0e965f9be61ac7711b52b6c0f
Ambroise ADANLEDJI <adanledjiambroise@gmail.com>
2023-02-16 16:55:42 +0100

    reservation from

--- brouillon_2.php
+++ brouillon_2.php
@@ -1,3 +1,5 @@
+<?php
+
 <?php  
 
 require_once '../config/database.php';
@@ -12,38 +14,53 @@ if(isset($_POST['reservation_btn'])){
             $pattern_name = "/^[a-zA-Z]+$/"; // accepte que alphabet et espace
             $pattern_phone = "/^[0-9]{10}$/"; // accepte que les chiffres
 
+            // Vérification du champ username
             if(empty($_POST['username'])){
                 $err_username = "Veuillez renseigner votre nom et prénom";
 
             }
+
+            // Vérification du format username
             if(!preg_match($pattern_name,$_POST['username'])){
                     $err_username_format = "Veuillez renseigner un nom et prénom valide";
 
             }
+
+            // Vérification du champ email
             if(empty($_POST['email'])){
                 $err_email = "Veuillez renseigner votre adresse mail";
                 
             }
+
+            // Vérification du format email
             if(!preg_match($pattern_email,$_POST['email'])){
                 $err_email_format = "Veuillez renseigner une adresse mail valide";
 
             }
+
+            // Vérification du champ phone
             if(empty($_POST['phone'])){
                 $err_phone = "Veuillez renseigner votre numéro de téléphone";
 
             }
-            if(empty($_POST['couvert'])){
-                $err_couvert = "Veuillez renseigner le nombre de couverts";
 
-            }
+            // Vérification du format phone
             if(!preg_match($pattern_phone,$_POST['phone'])){
                 $err_phone_format = "Veuillez renseigner un numéro de téléphone valide";
+            }
 
-            }    
+            //Vérification du champ couvert
+            if(empty($_POST['couvert'])){
+                $err_couvert = "Veuillez renseigner le nombre de couverts";
+            }
+
+            //Vérification du champ date
             if(empty($_POST['date'])){
                 $err_date = "Veuillez renseigner la date de votre réservation";
 
             }
+
+            //collecte des données desc hamps
 
             $username = $_POST['username'];
             $email = $_POST['email'];
@@ -52,29 +69,45 @@ if(isset($_POST['reservation_btn'])){
             $date = $_POST['date'];
             $allergies = $_POST['allergies'];
 
+            // Requête pour récupérer l'id de l'utilisateur
+
             $stmtGetUserId = $db->prepare("SELECT user_id FROM users WHERE user_email = :email");
             $stmtGetUserId->bindParam(":email", $email, PDO::PARAM_STR);
             $stmtGetUserId->execute();
-            $userId = $stmtGetUserId->fetch(PDO::FETCH_ASSOC);
+            $userId = $stmtGetUserId->fetch(PDO::FETCH_ASSOC)["user_id"];
+
+            echo "<pre>";
+            print_r($userId);
+            echo "</pre>";
+            
+
+            // Requête pour récupérer l'id de la réservation
 
             $stmtGetReservationId = $db->prepare("SELECT reservation_id FROM reservation WHERE reservation_datetime = :date");
             $stmtGetReservationId->bindParam(":date", $date, PDO::PARAM_STR);
             $stmtGetReservationId->execute();
-            $reservationId = $stmtGetReservationId->fetch(PDO::FETCH_ASSOC);
-            
+            $reservationId = $stmtGetReservationId->fetch(PDO::FETCH_ASSOC)["reservation_id"];
+            // Requête pour insérer les données dans la table users
+
+            echo "<pre>";
+            print_r($reservationId);
+            echo "</pre>";
 
             $reqUsers ="
-                INSERT INTO users (user_name, user_email, user_phone)VALUES (:username, :email, :phone);
-                
-            ";
+                INSERT INTO users (user_name, user_email, user_phone)VALUES (:username, :email, :phone)";
+
+            // Requête pour insérer les données dans la table reservation
             $reqReservation ="
                 INSERT INTO reservation (reservation_datetime, numberOfPeople, userId) 
-                VALUES (:date, :couvert, :userId);
-            ";
+                VALUES (:date, :couvert, :userId)";
+
+            // Requête pour insérer les données dans la table allergies
             $reqAllergies ="
                 INSERT INTO allergies (allergies_list, reservationId)
                 VALUES (:allergies, :reservationId);
             ";
+
+            // Préparation des requêtes
 
             $stmtUsers = $db->prepare($reqUsers);
             $stmtUsers->bindParam(":username", $username, PDO::PARAM_STR);
@@ -96,9 +129,13 @@ if(isset($_POST['reservation_btn'])){
 
                     if ($stmtAllergies->execute()) {
 
+                        //action à faire si la requête s'est bien exécutée
+
                         echo "Votre réservation a bien été prise en compte";
 
                     }else{
+
+                        //action à faire si la requête n'a pas pu s'exécuter
 
                         echo "Votre réservation n'a pas pu être enregistrer";
 
@@ -110,75 +147,8 @@ if(isset($_POST['reservation_btn'])){
     }
 }
 
-
+<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>
 
 
 ?>
-
-<?php
-
-
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    if (empty($_POST['username'])) {
-        $err_username = "Veuillez renseigner votre nom et prénom";
-    } else {
-        $username = test_input($_POST['username']);
-
-        if (!preg_match("/^[a-zA-Z ]*$/", $username)) {
-            $err_username_format = "Seules les lettres sont acceptées.";
-        }
-    }
-
-    if (empty($_POST['email'])) {
-        $err_email = "Veuillez renseigner votre adresse mail";
-    } else {
-        $email = test_input($_POST['email']);
-
-        if (!preg_match("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^",$email)) {
-            $err_email_format = "Veuillez renseigner une adresse mail valide";
-        }
-    }
-    
-    if (empty($_POST['phone'])){
-        $err_phone = "Veuillez renseigner votre numéro de téléphone";
-    } else {
-        $phone = test_input($_POST['phone']);
-
-        if (!preg_match("/^[0-9]{10}$/", $phone)) {
-            $err_phone_format = "Veuillez renseigner un numéro de téléphone valide";
-        }
-    }
-
-    if(empty($_POST['couvert'])){
-        $err_couvert = "Veuillez renseigner le nombre de couverts";
-    } else {
-        $couvert = test_input($_POST['couvert']);
-    }
-
-    if(empty($_POST['date'])){
-        $err_date = "Veuillez renseigner la date et l'heure de votre réservation";
-    } else {
-        $date = test_input($_POST['date']);
-    }
-
-    if(empty($_POST['allergies'])){
-        $err_allergies = "Veuiller saisir 'aucune', si vous n'avez pas d'allergies";
-    } else {
-        $allergies = test_input($_POST['allergies']);
-    }
-
-    function test_input($data)
-    {
-        $data = trim($data);
-        $data = stripcslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    };
-}
-?>
-
-
-
-
-
 
--- views/reservation.php
+++ views/reservation.php
@@ -3,150 +3,146 @@
 ob_start();
 ?>
 
-<?php  
+<?php
 
 require_once '../config/database.php';
 
-if(isset($_POST['reservation_btn'])){
-    if($_SERVER['REQUEST_METHOD'] == "POST"){
-
-        if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['phone'])
+if ($_SERVER['REQUEST_METHOD'] == "POST") 
+{
+    
+    if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['phone'])
         && isset($_POST['couvert']) && isset($_POST['date']) && isset($_POST['allergies']))
         {
-            $pattern_email = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^";//vérifie le format d'une adresse mail
-            $pattern_name = "/^[a-zA-Z]+$/"; // accepte que alphabet et espace
-            $pattern_phone = "/^[0-9]{10}$/"; // accepte que les chiffres
+    $pattern_email = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^";//vérifie le format d'une adresse mail
+    $pattern_name = "/^[a-zA-Z]+$/"; // accepte que alphabet et espace
+    $pattern_phone = "/^[0-9]{10}$/"; // accepte que les chiffres
 
-            // Vérification du champ username
-            if(empty($_POST['username'])){
-                $err_username = "Veuillez renseigner votre nom et prénom";
+    // Vérification du champ username
+    if (empty($_POST['username'])) {
+        $err_username = "Veuillez renseigner votre nom et prénom";
+    }
 
-            }
+    // Vérification du format username
+    if (!preg_match($pattern_name, $_POST['username'])) {
+        $err_username_format = "Veuillez renseigner un nom et prénom valide";
+    }
 
-            // Vérification du format username
-            if(!preg_match($pattern_name,$_POST['username'])){
-                    $err_username_format = "Veuillez renseigner un nom et prénom valide";
+    // Vérification du champ email
+    if (empty($_POST['email'])) {
+        $err_email = "Veuillez renseigner votre adresse mail";
+    }
 
-            }
+    // Vérification du format email
+    if (!preg_match($pattern_email, $_POST['email'])) {
+        $err_email_format = "Veuillez renseigner une adresse mail valide";
+    }
 
-            // Vérification du champ email
-            if(empty($_POST['email'])){
-                $err_email = "Veuillez renseigner votre adresse mail";
-                
-            }
+    // Vérification du champ phone
+    if (empty($_POST['phone'])) {
+        $err_phone = "Veuillez renseigner votre numéro de téléphone";
+    }
 
-            // Vérification du format email
-            if(!preg_match($pattern_email,$_POST['email'])){
-                $err_email_format = "Veuillez renseigner une adresse mail valide";
+    // Vérification du format phone
+    if (!preg_match($pattern_phone, $_POST['phone'])) {
+        $err_phone_format = "Veuillez renseigner un numéro de téléphone valide";
+    }
 
-            }
+    //Vérification du champ couvert
+    if (empty($_POST['couvert'])) {
+        $err_couvert = "Veuillez renseigner le nombre de couverts";
+    }
 
-            // Vérification du champ phone
-            if(empty($_POST['phone'])){
-                $err_phone = "Veuillez renseigner votre numéro de téléphone";
+    //Vérification du champ date
+    if (empty($_POST['date'])) {
+        $err_date = "Veuillez renseigner la date de votre réservation";
+    }
 
-            }
+    //Vérification du champ allergies
+    if (empty($_POST['allergies'])) {
+        $err_allergies = "Veuillez renseigner 'aucune' si vous n'avez pas d'allergies";
+    }
 
-            // Vérification du format phone
+    //collecte des données desc hamps
 
-            if(!preg_match($pattern_phone,$_POST['phone'])){
-                $err_phone_format = "Veuillez renseigner un numéro de téléphone valide";
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $couvert = $_POST['couvert'];
+    $date = $_POST['date'];
+    $allergies = $_POST['allergies'];
 
-            }
+    // Requête pour récupérer l'id de l'utilisateur
 
-            //Vérification du champ couvert
-            if(empty($_POST['couvert'])){
-                $err_couvert = "Veuillez renseigner le nombre de couverts";
+    $stmtGetUserId = $db->prepare("SELECT user_id FROM users WHERE user_email = :email");
+    $stmtGetUserId->bindParam(":email", $email, PDO::PARAM_STR);
+    $stmtGetUserId->execute();
+    $userId = $stmtGetUserId->fetch(PDO::FETCH_ASSOC)['user_id'];
 
-            }
+    // echo gettype($userId);
 
-            //Vérification du champ date
-            if(empty($_POST['date'])){
-                $err_date = "Veuillez renseigner la date de votre réservation";
+    // echo "<pre>";
+    // print_r($userId);
+    // echo "</pre>";
 
-            }
+    // Requête pour récupérer l'id de la réservation
+    $stmtGetReservationId = $db->prepare("SELECT reservation_id FROM reservation WHERE numberOfPeople = :numberOfPeople");
+    $stmtGetReservationId->bindParam(":couvert", $couvert, PDO::PARAM_STR);
+    $stmtGetReservationId->execute();
+    $reservationId = $stmtGetReservationId->fetch(PDO::FETCH_ASSOC);
 
-            //collecte des données desc hamps
+    //echo gettype($reservationId);
 
-            $username = $_POST['username'];
-            $email = $_POST['email'];
-            $phone = $_POST['phone'];
-            $couvert = $_POST['couvert'];
-            $date = $_POST['date'];
-            $allergies = $_POST['allergies'];
+    
+    var_dump ($reservationId);
+    
 
-            // Requête pour récupérer l'id de l'utilisateur
+    // Requête pour insérer les données dans la table users
 
-            $stmtGetUserId = $db->prepare("SELECT user_id FROM users WHERE user_email = :email");
-            $stmtGetUserId->bindParam(":email", $email, PDO::PARAM_STR);
-            $stmtGetUserId->execute();
-            $userId = $stmtGetUserId->fetch(PDO::FETCH_ASSOC);
-
-            // Requête pour récupérer l'id de la réservation
-
-            $stmtGetReservationId = $db->prepare("SELECT reservation_id FROM reservation WHERE reservation_datetime = :date");
-            $stmtGetReservationId->bindParam(":date", $date, PDO::PARAM_STR);
-            $stmtGetReservationId->execute();
-            $reservationId = $stmtGetReservationId->fetch(PDO::FETCH_ASSOC)["reservation_id"];
-            // Requête pour insérer les données dans la table users
-
-            $reqUsers ="
+    $reqUsers ="
                 INSERT INTO users (user_name, user_email, user_phone)VALUES (:username, :email, :phone)";
 
-            // Requête pour insérer les données dans la table reservation
-            $reqReservation ="
+    // Requête pour insérer les données dans la table reservation
+    $reqReservation ="
                 INSERT INTO reservation (reservation_datetime, numberOfPeople, userId) 
                 VALUES (:date, :couvert, :userId)";
 
-            // Requête pour insérer les données dans la table allergies
-            $reqAllergies ="
+    // Requête pour insérer les données dans la table allergies
+    $reqAllergies ="
                 INSERT INTO allergies (allergies_list, reservationId)
                 VALUES (:allergies, :reservationId);
             ";
 
-            // Préparation des requêtes
+    // Préparation des requêtes Users
 
-            $stmtUsers = $db->prepare($reqUsers);
-            $stmtUsers->bindParam(":username", $username, PDO::PARAM_STR);
-            $stmtUsers->bindParam(":email", $email, PDO::PARAM_STR);
-            $stmtUsers->bindParam(":phone", $phone, PDO::PARAM_INT);
+    $stmtUsers = $db->prepare($reqUsers);
+    $stmtUsers->bindParam(":username", $username, PDO::PARAM_STR);
+    $stmtUsers->bindParam(":email", $email, PDO::PARAM_STR);
+    $stmtUsers->bindParam(":phone", $phone, PDO::PARAM_INT);
 
-            if($stmtUsers->execute()){
+    // Préparation des requêtes Reservation
+    $stmtReservation = $db->prepare($reqReservation);
+    $stmtReservation->bindParam(":date", $date, PDO::PARAM_STR);
+    $stmtReservation->bindParam(":couvert", $couvert, PDO::PARAM_INT);
+    $stmtReservation->bindParam(":userId", $userId, PDO::PARAM_INT);
 
-                $stmtReservation = $db->prepare($reqReservation);
-                $stmtReservation->bindParam(":date", $date, PDO::PARAM_STR);
-                $stmtReservation->bindParam(":couvert", $couvert, PDO::PARAM_INT);
-                $stmtReservation->bindParam(":userId", $userId, PDO::PARAM_INT);
+    // Préparation des requêtes Allergies
+    $stmtAllergies = $db->prepare($reqAllergies);
+    $stmtAllergies->bindParam(":allergies", $allergies, PDO::PARAM_STR);
+    $stmtAllergies->bindParam(":reservationId", $reservationId, PDO::PARAM_INT);
 
-                if($stmtReservation->execute()){
+    // Exécution des requêtes
 
-                    $stmtAllergies = $db->prepare($reqAllergies);
-                    $stmtAllergies->bindParam(":allergies", $allergies, PDO::PARAM_STR);
-                    $stmtAllergies->bindParam(":reservationId", $reservationId, PDO::PARAM_INT);
+    if ($stmtUsers->execute() && $stmtReservation->execute() && $stmtAllergies->execute()) {
+        //action à faire si la requête s'est bien exécutée
 
-                    if ($stmtAllergies->execute()) {
+        echo "Votre réservation a bien été prise en compte";
+    } else {
+        //action à faire si la requête n'a pas pu s'exécuter
 
-                        //action à faire si la requête s'est bien exécutée
-
-                        echo "Votre réservation a bien été prise en compte";
-
-                    }else{
-
-                        //action à faire si la requête n'a pas pu s'exécuter
-
-                        echo "Votre réservation n'a pas pu être enregistrer";
-
-                    }
-                }
-            }
-        }
-
+        echo "Votre réservation n'a pas pu être enregistrer";
     }
 }
-
-
-
-
+}
 ?>
 
 
@@ -158,12 +154,13 @@ if(isset($_POST['reservation_btn'])){
                     cadre exceptionnel pour des moments inoubliables.</p>
             </div>
             <div class="container_reservation_form">
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <form action="" method="post">
 
                 <label for="username"></label>
                 <input type="text" id="username" name="username" placeholder="Votre nom et prénom">
                 
                 <p class="error"><?php echo isset($err_username) ? $err_username: "";?></p>
+                <p class="error"><?php echo isset($err_username_format) ? $err_username_format: "";?></p>
                 
                 
                 <label for="email"></label>
@@ -186,6 +183,7 @@ if(isset($_POST['reservation_btn'])){
 
                 
                 <textarea name="allergies" id="allergies" cols="10" rows="10" placeholder="Des allergies ?"></textarea>
+                <p class="error"><?php echo isset($err_allergies) ? $err_allergies: "";?></p>
                 <div class="reservation_btn">
                     <input type="submit" name="reservation_btn" value="Réserver">
                 </div>
